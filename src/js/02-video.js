8a813d0963b27ff9db3beaf7019c34e5fa01104f
VladyslavYakivets <aquartis97@gmail.com>
2023-03-11 10:17:07 +0200

    друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const VIMEO_KEY_LS = 'videoplayer-current-time';
+const setWatchingTime = ({ seconds }) => {
+  localStorage.setItem(VIMEO_KEY_LS, seconds);
+};
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+player.setCurrentTime(localStorage.getItem(VIMEO_KEY_LS) || 0);
+
+
 
